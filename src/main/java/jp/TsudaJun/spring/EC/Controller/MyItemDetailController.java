bc18d91f3e16989afd8222a74b9c480cf20fe37d
津田準 <TsudaJun@tsudajun-no-MacBook-Air.local>
2018-03-18 18:46:02 +0900

    commit

--- src/main/java/jp/TsudaJun/spring/EC/Controller/MyItemDetailController.java
+++ src/main/java/jp/TsudaJun/spring/EC/Controller/MyItemDetailController.java
@@ -1,8 +1,12 @@
 package jp.TsudaJun.spring.EC.Controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.security.Principal;
 import java.util.List;
+
+import javax.servlet.annotation.MultipartConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import jp.TsudaJun.spring.EC.DAO.ItemAttributeDao;
@@ -22,6 +27,8 @@ import jp.TsudaJun.spring.EC.model.Item;
 import jp.TsudaJun.spring.EC.model.ItemAttribute;
 
 @Controller
+@MultipartConfig(location="/tmp", fileSizeThreshold=1024*1024, 
+maxFileSize=1024*1024*5, maxRequestSize=1024*1024*5*5)
 public class MyItemDetailController {
 	
 	@Autowired
@@ -32,6 +39,9 @@ public class MyItemDetailController {
 	
 	@Value("${img.accessPath}")
 	private String imgPath;
+	
+	@Value("${img.path}")
+	private String imgSavePath;
 	
 	@RequestMapping(value ="/myitemdetail/{itemid}", method=RequestMethod.GET)
 	public ModelAndView show(
@@ -52,23 +62,33 @@ public class MyItemDetailController {
 		return mav;
 	}
 	
-	@RequestMapping(value ="/myitemdetail/{itemid}", method=RequestMethod.POST)
+	@RequestMapping(value ="/myitemdetail/{itemid}", method=RequestMethod.POST, produces="text/plain;charset=UTF-8")
 	public ModelAndView register(
 			ModelAndView mav,
 			@Validated @ModelAttribute("item") Item item,
 			BindingResult result,
 			@PathVariable("itemid") int itemid,
-			Principal principal) {
+			@RequestParam MultipartFile file,
+			Principal principal) throws IllegalStateException, IOException {
 		mav.setViewName("myitemdetail");
 		if(!result.hasErrors()) {
+			//ID
 			item.setItemid(itemid);
+			//includingtax
 			BigDecimal price_bd = new BigDecimal(String.valueOf(item.getPrice()));
 			BigDecimal tax_rate = new BigDecimal(0.08);
 			BigDecimal tax = price_bd.multiply(tax_rate).setScale(0, BigDecimal.ROUND_HALF_UP);
 			BigDecimal includingtax_bd = price_bd.add(tax);
 			item.setTax(tax.intValue());
 			item.setIncludingtax(includingtax_bd.intValue());
+			//画像
+			if (file.getSize() > 0 && file.getContentType().equals("image/jpeg") ) {
+				File imageFile = new File(imgSavePath + item.getItemid() +".jpg");
+				file.transferTo(imageFile);
+			}
+			//userid
 			item.setUserid(principal.getName());
+			//merge
 			iDao.merge(item);
 			iDao.close();
 			mav = new ModelAndView("redirect:/kanryo/?msg=myitemdetail");
